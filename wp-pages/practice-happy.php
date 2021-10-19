5cff4de83c0f4f047bcf4f7b9eb89a09726f9054
Zafor Iqbal <zaforiqbal13@gmail.com>
2021-10-19 17:11:55 +0600

    practice happy landing page and template

---
+++ wp-pages/practice-happy.php
@@ -0,0 +1,115 @@
+<?php
+/**
+ * Template Name: Practice Happy Landing Page
+ */ 
+?>
+
+<div class="container coreplus-practice-happy-landing-page">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="coreplus-ph-hero-text">
+            <h2 class="coreplus-ph-hero-header">
+              Be part of the Practice Happy community
+            </h2>
+            <p>
+              Where our customers come together to connect, learn, and share
+              with other allied healthcare professionals.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6 cp-lp-hero-image">
+          <img src="<?php get_template_directory_uri(). '/practicehappy/images/practice-happy-hero-image.svg' ?>" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="container ph-logo-line">
+      <div class="row">
+        <div class="col-md-4"><hr class="ph-line ph-hide-in-small" /></div>
+        <div class="col-md-4 ph-logo">
+          <img src="<?php get_template_directory_uri().'/practicehappy/images/practice-happy-logo.png'?>" alt="" />
+        </div>
+        <div class="col-md-4"><hr class="ph-line ph-hide-in-small" /></div>
+      </div>
+    </div>
+
+    <div class="container practice-happy-community">
+      <div class="row">
+        <div class="col-md-6 phc-text-div">
+          <div class="phc-text">
+            <p>
+              coreplus exists to connect, help & grow Allied Healthcare
+              professionals with digital first healthcare service experiences
+              for their clients, so it only made sense that we created a
+              community to support this vision.
+              <br />
+              <br />
+              Practice Happy is much more than a help forum. It’s a community
+              that you'll enjoy being part of because of the valuable content,
+              real conversations and meaningful connections.
+            </p>
+          </div>
+          <div class="phc-button">
+            <a href="https://practicehappy.coreplus.com.au/"
+              >Practice Happy Community</a
+            >
+          </div>
+        </div>
+        <div class="col-md-6 phc-image">
+          <img src="<?php get_template_directory_uri().'/practicehappy/images/practice-happy-mockup.png'?>" alt="" />
+        </div>
+      </div>
+    </div>
+
+    <div class="container practice-happy-divider">
+      <div class="row">
+        <hr class="ph-line" />
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="row">
+        <h1 class="phb-title">
+          What are the benefits of being a Practice <br />
+          Happy community member?
+        </h1>
+      </div>
+    </div>
+
+    <div class="container ph-benefits-section">
+      <div class="row">
+        <div class="col-md-4">
+          <div class="ph-benefits">
+            <img src="<?php get_template_directory_uri().'/practicehappy/images/ph-connect.svg'?>" alt="" />
+            <h4>Connect Together</h4>
+            <p>
+              meet, have conversations and build relationships with like-minded
+              allied health care professionals from all over Australiay
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ph-benefits">
+            <img src="<?php get_template_directory_uri().'/practicehappy/images/learn-together.svg'?>" alt="" />
+            <h4>Learn Together</h4>
+            <p>
+              expand your knowledge with our special educational events, as well
+              as content and discussions in our ‘Success Tips’ and ‘Tech Talk’
+              groups
+            </p>
+          </div>
+        </div>
+        <div class="col-md-4">
+          <div class="ph-benefits">
+            <img src="<?php get_template_directory_uri().'/practicehappy/images/grow-together.svg'?>" alt="" />
+            <h4>Grow Together</h4>
+            <p>
+              join profession or role specific ‘clubhouse’ groups where you can
+              ask for advice, share tips, and discuss industry-related news and
+              topics
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+
